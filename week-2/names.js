58571a80376d09a42fce1ad45437410cab935a97
Hannah Del Real <hdelreal@my365.bellevue.edu>
2023-01-13 00:10:33 -0800

    intial commit

---
+++ week-2/names.js
@@ -0,0 +1,14 @@
+const txtFirstNameLudwig = "Ludwig";
+const txtLastNameBeethoven = "Beethoven";
+const txtFirstNameJohann = "Johann";
+const txtLastNameBach = "Bach";
+const txtFirstNameWolfgang= "Wolfgang";
+const txtLastNameMozart = "Mozart";
+
+
+document.getElementById("txtFirstNameLudwig").innerHTML = txtFirstNameLudwig;
+document.getElementById("txtLastNameBeethoven").innerHTML = txtLastNameBeethoven;
+document.getElementById("txtFirstNameJohann").innerHTML = txtFirstNameJohann;
+document.getElementById("txtLastNameBach").innerHTML = txtLastNameBach;
+document.getElementById("txtFirstNameWolfgang").innerHTML = txtFirstNameWolfgang;
+document.getElementById("txtLastNameMozart").innerHTML = txtLastNameMozart;
